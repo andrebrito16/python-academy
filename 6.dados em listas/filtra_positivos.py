872ea1035874c487bbf9a1e7028d0b61e29d83cf
andrebrito16 <felipe.brito016@gmail.com>
2021-04-07 15:04:49 -0400

    apply filter to filter positive numbers on list

--- 6.dados em listas/filtra_positivos.py
+++ 6.dados em listas/filtra_positivos.py
@@ -1,7 +1,18 @@
+# def filtra_positivos(lista):
+#     # positivos = []
+#     for e in lista:
+#         if e < 0:
+#             lista.remove(e)
+
+#     return lista
+
+
+
 def filtra_positivos(lista):
-    positivos = []
-    for n in lista:
-        if n > 0:
-            positivos.append(n)
+    return list(filter(lambda x: x>0, lista))
 
     return positivos
+
+lista = [-2, 2, 3, 4, 5]
+
+print(filtra_positivos(lista))
